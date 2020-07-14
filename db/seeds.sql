8e831e13c10e47acd99484e8f2502ad79460618e
Rodia Raskolnikof <cvaz789@gmail.com>
2020-07-14 03:29:41 -0500

    Post Not working yet

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name) VALUES ("CHICKEN BURGER");
+INSERT INTO burgers(burger_name) VALUES ("VEGGIE BURGER");
+INSERT INTO burgers(burger_name) VALUES ("SHRIMP BURGER");
+INSERT INTO burgers(burger_name) VALUES ("SIRLOIN BURGER");
---
+++ public/assets/js/burger.js
@@ -0,0 +1,63 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#ca").val().trim(),
+        devoured: $("[name=hungry]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Ordered a new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+    $(".devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newState = $(this).data("newsleep");
+  
+      var newEatenState = {
+        devoured: newState
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("Eaten!", newState);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    // $(".delete-cat").on("click", function(event) {
+    //   var id = $(this).data("id");
+  
+    //   // Send the DELETE request.
+    //   $.ajax("/api/cats/" + id, {
+    //     type: "DELETE"
+    //   }).then(
+    //     function() {
+    //       console.log("deleted cat", id);
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+  });
+  
